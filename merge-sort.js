82e759a99035fd483708da3dccaea07bd9bdc12f
Tom Hennessy <tomlhennessy@outlook.com>
2024-08-01 20:35:28 +0100

    implemented merge sort functions

--- merge-sort.js
+++ merge-sort.js
@@ -1,34 +1,51 @@
 // Merge Sort out-of-place
 // Do not modify the original array
 function mergeSort(arr) {
+  // check if the input is length 1 or less
+  if (arr.length <= 1) {
+    // if so, it's already sorted: return
+    return arr;
+  }
 
-  // Check if the input is length 1 or less
-    // If so, it's already sorted: return
+  // divide the array in half
+  const mid = Math.floor(arr.length / 2);
 
-  // Divide the array in half
+  // recursively sort left half
+  const left = mergeSort(arr.slice(0, mid));
 
-  // Recursively sort the left half
-  // Recursively sort the right half
+  // recursively sort right half
+  const right = mergeSort(arr.slice(mid));
 
-  // Merge the halves together and return
-
+  // merge the halves together and return
+  return merge(left, right);
 }
 
 
 // Takes in two sorted arrays and returns them merged into one
 function merge(arrA, arrB) {
+  // create an empty return array
+  let result = [];
 
-  // Create an empty return array
+  // point to the first value of each array
+  let i = 0;
+  let j = 0;
 
-  // Point to the first value of each array
-  // While there are still values in each array...
-    // Compare the first values of each array
-    // Add the smaller value to the return array
-    // Move the pointer to the next value in that array
+  // while there are still values in each array...
+  while (i < arrA.length && j < arrB.length) {
+    // compare the first values of each array
+    if (arrA[i] < arrB[j]) {
+      // add the smaller value to the return array
+      result.push(arrA[i]);
+      // move the pointer to the next value in that array
+      i++;
+    } else {
+      result.push(arrB[j]);
+      j++;
+    }
+  }
 
-  // Return the return array
-
+  // concatenate remaining elements (one of these will be empty)
+  return result.concat(arrA.slice(i)).concat(arrB.slice(j));
 }
 
 module.exports = [merge, mergeSort];
-
